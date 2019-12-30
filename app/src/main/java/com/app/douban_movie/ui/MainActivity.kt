a5cbd00afac4c9a97da11794b905ca1d8a23a244
wangliqiang <wangliqiangorz@163.com>
2019-12-30 10:07:34 +0800

    添加动态权限获取

--- app/src/main/java/com/app/douban_movie/ui/MainActivity.kt
+++ app/src/main/java/com/app/douban_movie/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.app.douban_movie.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
@@ -10,14 +15,35 @@ import com.app.douban_movie.ui.fragments.FindFragment
 import com.app.douban_movie.ui.fragments.HotFragment
 import com.app.douban_movie.ui.fragments.MineFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    // 获取权限
+    private val REQUEST_CODE_PERMISSIONS = 101
+
+    private val KEY_PERMISSIONS_REQUEST_COUNT = "KEY_PERMISSIONS_REQUEST_COUNT"
+    private val MAX_NUMBER_REQUEST_PERMISSIONS = 2
+
+    private val permissions = listOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    private var permissionRequestCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initializeUI()
+
+        savedInstanceState?.let {
+            permissionRequestCount = it.getInt(KEY_PERMISSIONS_REQUEST_COUNT, 0)
+        }
+        // 获取权限
+        requestPermissionsIfNecessary()
     }
 
     private fun initializeUI() {
@@ -63,5 +89,55 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+    }
+
+
+    /**
+     * Request permissions twice - if the user denies twice then show a toast about how to update
+     * the permission for storage. Also disable the button if we don't have access to pictures on
+     * the device.
+     */
+    private fun requestPermissionsIfNecessary() {
+        if (!checkAllPermissions()) {
+            if (permissionRequestCount < MAX_NUMBER_REQUEST_PERMISSIONS) {
+                permissionRequestCount += 1
+                ActivityCompat.requestPermissions(
+                    this,
+                    permissions.toTypedArray(),
+                    REQUEST_CODE_PERMISSIONS
+                )
+            } else {
+                Toast.makeText(
+                    this,
+                    R.string.set_permissions_in_settings,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    /** Permission Checking  */
+    private fun checkAllPermissions(): Boolean {
+        var hasPermissions = true
+        for (permission in permissions) {
+            hasPermissions = hasPermissions and isPermissionGranted(permission)
+        }
+        return hasPermissions
+    }
+
+    private fun isPermissionGranted(permission: String) =
+        ContextCompat.checkSelfPermission(this, permission) ==
+                PackageManager.PERMISSION_GRANTED
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            requestPermissionsIfNecessary() // no-op if permissions are granted already.
+        }
     }
 }
