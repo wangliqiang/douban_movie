e44fe1da9601612ac62372a1b21c491e32ceffc9
wangliqiang <wangliqiangorz@163.com>
2020-01-06 16:04:53 +0800

    优化协程调度

--- app/src/main/java/com/app/douban_movie/ui/fragments/hot/ComingSoonFragment.kt
+++ app/src/main/java/com/app/douban_movie/ui/fragments/hot/ComingSoonFragment.kt
@@ -2,14 +2,13 @@ package com.app.douban_movie.ui.fragments.hot
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.app.douban_movie.data.model.Theaters
-
 import com.app.douban_movie.databinding.FragmentComingSoonBinding
 import com.app.douban_movie.ui.adapters.ComingSoonAdapter
 import com.app.douban_movie.ui.adapters.decoration.HeaderDecoration
